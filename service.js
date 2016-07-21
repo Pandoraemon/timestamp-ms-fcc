ed03899cc663c609c8bac31dad1a47d133008ccb
pandoraemon <pz880304@163.com>
2016-07-21 22:37:15 +0800

    fix

--- service.js
+++ service.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res){
     res.send('hello World');
 });
 
-var server = app.listen(8080, function(){
+var server = app.listen(app.get('port'), function(){
 
     var host = server.address().address;
     var port = server.address().port;
-    console.log("listen on http://%s:%s", host, port)
+    console.log("listen on http://%s:%s", host, port);
 });
