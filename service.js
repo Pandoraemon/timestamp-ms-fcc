73895455b128afcd7a1270c48492e7637c1c78b5
pandoraemon <pz880304@163.com>
2016-07-24 11:20:06 +0800

    add home page

--- service.js
+++ service.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/', function(req, res){
+  res.send("<h2>Example usage:</h2><p>https://timestamp-ms.herokuapp.com/December%2015,%202015</p><p>https://timestamp-ms.herokuapp.com/1450137600</p><h2>Example output:</h2><p>{ 'unix': 1450137600, 'natural': 'December 15, 2015' }</p>");
+
+});
 app.get('/:time', function(req, res){
   var date = req.params.time;
   if (/^[0-9]*$/.test(date))
